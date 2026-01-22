e1bbca14d2cb68467ac2f4f1f63e94b1b7f668fc
RawanYasser297 <rawanyasser980@gmail.com>
2026-01-22 21:06:40 +0200

    remove btn

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -62,14 +62,9 @@ const Contact = () => {
         </div>
         
       
-        <Button size="lg" className="glowingButton" asChild>
-          <a href="mailto:rawan2972000@gmail.com">
-            <Mail className="w-5 h-5 mr-2" />
-            {t("contact.send")}
-          </a>
-        </Button>
+       
 
-        {/* Footer */}
+        
         <div className="mt-24 pt-12 border-t border-border">
           <p className="text-muted-foreground">{t("contact.copyright")}</p>
         </div>
